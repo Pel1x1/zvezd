6a2cfea6ad53ffc60320a6a6c79e61a6cdf6bf54
Kostya <k.konstantin2212@gmail.com>
2025-06-07 19:24:35 +0300

    Update Hero.tsx

--- src/app/events/sections/Hero.tsx
+++ src/app/events/sections/Hero.tsx
@@ -24,7 +24,7 @@ export const Hero = () => {
       <div className="items-center flex flex-col justify-center">
       <p className="text-white text-[30px] md:text-[70px] mb-4 drop-shadow-lg" style={{fontFamily: "ZenAntoquie"}}>Мероприятия</p>
       <p className="text-white text-[15px] md:text-[30px] font-light max-w-4xl text-center drop-shadow-md"  style={{fontFamily: "RobotoL"}}>
-      Присоединяйтесь к нашим эксклюзивным мероприятиям.<br/> Каждое событие - это уникальная возможность насладиться особенной атмосферой и создать незабываемые воспоминания.
+      Присоединяйтесь к нашим эксклюзивным мероприятиям.<br/> Каждое событие - это уникальная возможность насладиться<br/> особенной атмосферой и создать незабываемые воспоминания.
       </p>
       </div>
     </div>
